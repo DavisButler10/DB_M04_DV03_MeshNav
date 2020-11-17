f01fa59077247457a8569e60d04fb968bfe093a8
DavisButler10 <45404502+DavisButler10@users.noreply.github.com>
2020-11-16 21:08:28 -0700

    Add files via upload

---
+++ Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI mainText;
+    public int pillarNum;
+
+    public static GameManager Karen;
+
+    private void Start()
+    {
+        Karen = this;
+        mainText.text = "Knock off all the pillars to win!";
+    }
+
+    private void Update()
+    {
+        scoreText.text = "Pillars Left: " + pillarNum;
+        if(Time.timeSinceLevelLoad > 10f)
+        {
+            mainText.enabled = false;
+        }
+        if(pillarNum <= 0)
+        {
+            mainText.enabled = true;
+            mainText.text = "Wow! Incredible! yOu ArE a TrUe GaMeR!";
+        }
+    }
+}
---
+++ Scripts/Pillars.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pillars : MonoBehaviour
+{
+    void Update()
+    {
+        if (transform.position.y < -5f)
+        {
+            GameManager.Karen.pillarNum--;
+            Destroy(gameObject);
+        }
+    }
+}
